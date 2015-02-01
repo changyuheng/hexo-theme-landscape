f2793823ff28fc37e792136b73c8e756edaaf91a
Chang Yu-heng <mr.changyuheng@gmail.com>
2015-02-01 18:53:27 +0800

    Ignore images within 'do not render' class

--- source/js/script.js
+++ source/js/script.js
@@ -89,6 +89,7 @@
   $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
       if ($(this).parent().hasClass('fancybox')) return;
+      if ($(this).hasClass('do-not-render')) return;
 
       var alt = this.alt;
 
